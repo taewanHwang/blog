77af214b88a5c743d87a7206396c7d8b1d774d4d
taewan <michel4546@gmail.com>
2012-07-28 21:25:53 -0700

    mongo DB start 하고 run되게 수정.

--- app.js
+++ app.js
@@ -1,53 +1,57 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , http = require('http')
-  , path = require('path');
+var express = require('express');
+var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
 
-var app = express();
-var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
+
+var app = module.exports = express.createServer();
+
+// Configuration
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
+  // using csrf
   app.use(express.cookieParser());
-  app.use(express.session({ secret: "changeme" }));
+  app.use(express.session({ secret: "secret" }));
+
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(express.static(__dirname + '/public'));
+  //csrf
   app.use(express.csrf());
 });
+
 app.configure('development', function(){
-  app.use(express.errorHandler());
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
 });
 
-var articleProvider= new ArticleProvider('localhost',27017);
+var articleProvider = new ArticleProvider('localhost', 27017);
+// Routes
 
 app.get('/', function(req, res){
     articleProvider.findAll( function(error,docs){
-        res.render('index.jade',{
-            title: 'Blog',
-            articles:docs
+        res.render('index.jade', { 
+	        title: 'Blog',
+	        articles:docs
         });
     })
 });
-app.get('/blog/new', function(req, res){
+
+app.get('/blog/new', function(req, res) {
     res.render('blog_new.jade', {
         title: 'New Post',
 		token:req.session._csrf
     });
 });
+
 app.post('/blog/new', function(req, res){
     articleProvider.save({
         title: req.param('title'),
@@ -56,23 +60,24 @@ app.post('/blog/new', function(req, res){
         res.redirect('/')
     });
 });
-app.get('/blog/:id',function(req,res){
-	articleProvider.findById(req,params.id,function(error,article){
-		res.render('blog_show.jade',
-		{locals:{
-			title:article.title,
-			article:article
-		}}
-		);
-	})
-})
-app.post('/blog/addComment',function(req,res){
-	articleProvider.addCommentToArticle(req.params('_id'),{
-		person:req.params('person'),
-		comment:req.params('comment'),
-		created_at:new Date()
-	},function(error,docs){
-		res.redirect('/blog/'+req.params('_id'))
-	})
-})
-//console.log("express server listening on port %d in %s mode",app.address().port,app.setting.env);
+
+app.get('/blog/:id', function(req, res) {
+    articleProvider.findById(req.params.id, function(error, article) {
+        res.render('blog_show.jade', {
+            title: article.title,
+            article:article
+        });
+    });
+});
+
+app.post('/blog/addComment', function(req, res) {
+    articleProvider.addCommentToArticle(req.param('_id'), {
+        person: req.param('person'),
+        comment: req.param('comment'),
+        created_at: new Date()
+       } , function( error, docs) {
+           res.redirect('/blog/' + req.param('_id'))
+       });
+});
+
+app.listen(3000);
