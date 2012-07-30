e801e138e926820f6fd20c8ad4f58253f9351a98
taewan <michel4546@gmail.com>
2012-07-30 12:06:18 -0700

    route 기능을 route.js 분리.

--- app.js
+++ app.js
@@ -3,8 +3,7 @@
  */
 
 var express = require('express'),
-	path = require('path');
-var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
+	routes = require('./routes');
 
 
 var app = module.exports = express.createServer();
@@ -16,13 +15,13 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.bodyParser({uploadDir:'./uploads'}));
   app.use(express.methodOverride());
-  // using csrf
+  // setting for csrf
   app.use(express.cookieParser());
   app.use(express.session({ secret: "secret" }));
 
   app.use(require('stylus').middleware({ src: __dirname + '/public' }));
   app.use(express.static(__dirname + '/public'));
-  //csrf
+
   app.use(express.csrf());
 });
 
@@ -33,69 +32,10 @@ app.configure('development', function(){
 app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
-
-var articleProvider = new ArticleProvider('localhost', 27017);
-// Routes
-
-app.get('/', function(req, res){
-    articleProvider.findAll( function(error,docs){
-        res.render('index.jade', { 
-	        title: 'Blog',
-	        articles:docs
-        });
-    })
-});
-
-app.get('/blog/new', function(req, res) {
-    res.render('blog_new.jade', {
-        title: 'New Post',
-		token:req.session._csrf
-    });
-});
-
-app.post('/blog/new', function(req, res){
-    articleProvider.save({
-        title: req.param('title'),
-        body: req.param('body')
-    }, function( error, docs) {
-        res.redirect('/')
-    });
-});
-
-app.get('/blog/:id', function(req, res) {
-    articleProvider.findById(req.params.id, function(error, article) {
-        res.render('blog_show.jade', {
-            title: article.title,
-            article:article,
-			token: req.session._csrf,
-        });
-    });
-});
-
-app.post('/blog/addComment', function(req, res) {
-    articleProvider.addCommentToArticle(req.param('_id'), {
-        person: req.param('person'),
-        comment: req.param('comment'),
-        created_at: new Date()
-       } , function( error, docs) {
-           res.redirect('/blog/' + req.param('_id'))
-       });
-});
-// we need the fs module for moving the uploaded files
-var fs = require('fs');
-app.post('/file-upload', function(req, res) {
-    // get the temporary location of the file
-    var tmp_path = req.files.thumbnail.path;
-    // set where the file should actually exists - in this case it is in the "images" directory
-    var target_path = path.join(__dirname,'images',req.files.thumbnail.name);
-    // move the file from the temporary location to the intended location
-    fs.rename(tmp_path, target_path, function(err) {
-        if (err) throw err;
-        // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
-        fs.unlink(tmp_path, function() {
-            if (err) throw err;
-            res.send('File uploaded to: ' + target_path + ' - ' + req.files.thumbnail.size + ' bytes');
-        });
-    });
-});
+app.get('/', routes.index);
+app.get('/blog/new',routes.blog.new.get);
+app.post('/blog/new',routes.blog.new.post);
+app.get('/blog/:id', routes.blog.id);
+app.post('/blog/addComment', routes.blog.addComment);
+app.post('/file-upload', routes.fileUpload);
 app.listen(3000);
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,73 @@
 /*
  * GET home page.
  */
+var ArticleProvider = require('../articleprovider-mongodb').ArticleProvider;
+var articleProvider = new ArticleProvider('localhost', 27017);
+var fs = require('fs');
+var path = require('path');
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+    articleProvider.findAll( function(error,docs){
+        res.render('index.jade', { 
+	        title: 'Blog',
+	        articles:docs
+        });
+    })
 };
+exports.blog = {
+	new :{
+		get:function(req,res){
+		    res.render('blog_new.jade', {
+		        title: 'New Post',
+				token:req.session._csrf
+		    });
+		},
+		post:function(req,res){
+			articleProvider.save({
+		        title: req.param('title'),
+		        body: req.param('body')
+		    }, function( error, docs) {
+		        res.redirect('/')
+		    });
+		}
+	},
+	id:function(req,res){
+	    articleProvider.findById(req.params.id, function(error, article) {
+	        res.render('blog_show.jade', {
+	            title: article.title,
+	            article:article,
+				token: req.session._csrf,
+	        });
+	    });		
+	},
+	addComment:function(req,res){
+    	articleProvider.addCommentToArticle(req.param('_id'), {
+	        person: req.param('person'),
+	        comment: req.param('comment'),
+	        created_at: new Date()
+	        } , function( error, docs) {
+	             res.redirect('/blog/' + req.param('_id'))
+            });
+		}
+}
+exports.fileUpload = function(req,res){
+    // get the temporary location of the file
+    var tmp_path = req.files.thumbnail.path;
+    // set where the file should actually exists - in this case it is in the "images" directory
+    var target_path = path.join(path.resolve(__dirname,'..'),'images',req.files.thumbnail.name);
+	console.log(tmp_path+'&'+target_path);
+    // move the file from the temporary location to the intended location
+    fs.rename(tmp_path, target_path, function(err) {
+        if (err) throw err;
+        // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+        fs.unlink(tmp_path, function() {
+            if (err) throw err;
+            res.send('File uploaded to: ' + target_path + ' - ' + req.files.thumbnail.size + ' bytes');
+        });
+    });
+	
+}
+
+/*
+// we need the fs module for moving the uploaded files
+*/
